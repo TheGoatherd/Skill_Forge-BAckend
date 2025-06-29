bb80957f21a0dc8620c90a2e237b056173e8369d
TheGoatherd <iamankit4435@gmail.com>
2025-06-30 01:45:27 +0530

    upgrade

--- app/database/mongodb.py
+++ app/database/mongodb.py
@@ -4,6 +4,6 @@ from dotenv import load_dotenv
 
 load_dotenv() # Load .env variables
 
-MONGO_URI =os.getenv("MONGO_URI","mongodb+srv://iamankit4435:@Totaloverdose@skillforge-database.vwfthj6.mongodb.net/skillforge?retryWrites=true&w=majority")
+MONGO_URI =os.getenv("MONGO_URI","mongodb+srv://iamankit4435:Totaloverdose@skillforge-database.vwfthj6.mongodb.net/skillforge?retryWrites=true&w=majority")
 client=motor.motor_asyncio.AsyncIOMotorClient(MONGO_URI)
 db = client["skillforge_database"] 
